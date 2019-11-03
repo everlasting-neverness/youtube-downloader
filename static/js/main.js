94e29278d7464da5224812b285cfe61f0697f6ba
Vasily <gerasimovv1990@gmail.com>
2019-11-03 07:31:26 +0300

    update

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,27 @@
 window.onload = function() {
     const downloadForm = document.querySelector('.form');
     const urlInput = this.document.getElementById('url_to_download');
+
+    function download_file(name, contents) {
+        var mime_type = "audio/mpeg";
+
+        var blob = new Blob([contents], {type: mime_type});
+
+        var dlink = document.createElement('a');
+        dlink.download = name;
+        dlink.href = window.URL.createObjectURL(blob);
+        dlink.onclick = function(e) {
+            // revokeObjectURL needs a delay to work properly
+            var that = this;
+            setTimeout(function() {
+                window.URL.revokeObjectURL(that.href);
+            }, 1500);
+        };
+
+        dlink.click();
+        dlink.remove();
+    }
+
     downloadForm.addEventListener('submit', e => {
         e.preventDefault();
         const urlToDownload = urlInput.value;
@@ -10,6 +31,8 @@ window.onload = function() {
         fetch(`/download?url_to_download=${urlToDownload}`)
             .then(res => {
                 console.log(res);
+                // return res;
+                download_file('test', res);
             })
             .catch(err => {
                 console.log(err);
--- web_app.py
+++ web_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, send_file
+from flask import Flask, render_template, request, redirect, send_file, make_response
 from downloader import downloader
 import os
 
@@ -8,14 +8,20 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+
 @app.route('/download', methods=['GET'])
-def download():
+def upload_link():
     url_to_download = username = request.args.get('url_to_download')
     if url_to_download == '':
         return redirect('/')
     else:
         output_file_name = downloader({ 'url': url_to_download})
-        return send_file(os.path.join('Files', output_file_name), attachment_filename=output_file_name)
+        # return output_file_name
+        response = make_response(send_file(os.path.join('Files', output_file_name), attachment_filename=output_file_name, as_attachment=True))
+        response.headers['Access-Control-Expose-Headers'] = 'Content-Disposition'
+        response.headers['X-file-name'] = output_file_name
+        return response
+
 
 @app.route('/error_503', methods=['GET'])
 def error_503():
